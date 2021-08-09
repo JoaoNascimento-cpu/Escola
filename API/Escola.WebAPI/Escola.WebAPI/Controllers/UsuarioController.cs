67f0faf2a116102bdc95039e4b6c0949e5e6e32b
Joao Nascimento <jhonyzito4gb@gmail.com>
2021-08-09 14:21:22 -0300

    api V:1.48 - todas as entidades feitas e banco atualizado

---
+++ API/Escola.WebAPI/Escola.WebAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,84 @@
+﻿using Escola.WebAPI.Domains;
+using Escola.WebAPI.Interfaces;
+using Escola.WebAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Escola.WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private IUsuarioRepository usuario { get; set; }
+
+        public UsuarioController()
+        {
+            usuario = new UsuarioRepository();
+        }
+
+        [HttpGet]
+        //http://5000/api/usuario
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(usuario.Listar());
+            }
+            catch (Exception exception)
+            {
+
+                return BadRequest(exception);
+            }
+        }
+        
+        [HttpPost]
+        public IActionResult Cadastro(Usuario novoUsuario)
+        {
+            try
+            {
+                usuario.Cadastro(novoUsuario);
+                return StatusCode(201);
+            }
+            catch (Exception exception)
+            {
+
+                return BadRequest(exception);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                usuario.Deletar(id);
+                return StatusCode(204);
+            }
+            catch (Exception exception)
+            {
+
+                return BadRequest(exception);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Usuario novoUsuario)
+        {
+            try
+            {
+                usuario.AtualizarUsuario(id, novoUsuario);
+                return StatusCode(204);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception);
+            }
+        }
+    }
+}
